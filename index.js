ee881726957507c845181376a6163f69d63658e2
fujiya228 <fujiya0xE2@gmail.com>
2023-10-14 02:05:48 +0900

    DBの情報を読み取り
    データベースに接続して、sqlite_master テーブルから各テーブルの名前とその作成時のSQLを取得
    
    ロックがかかっており直接読み込めなかったので、ファイルをコピーして読み込み

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 const fs = require('fs');
 const os = require('os');
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -17,12 +19,35 @@ app.whenReady().then(() => {
   const username = os.userInfo().username;
   console.log(username);
 
-  fs.readFile(`/Users/${username}/Library/Application Support/Google/Chrome/Default/History`, 'utf8', (err, data) => {
+  const dbPath = `/Users/${username}/Library/Application Support/Google/Chrome/Default/History`;
+  const tempPath = path.join(__dirname, 'tempHistoryCopy');
+
+  fs.copyFileSync(dbPath, tempPath);
+
+  const db = new sqlite3.Database(tempPath, sqlite3.OPEN_READONLY, (err) => {
     if (err) {
-      console.error('An error occurred:', err);
+      console.error(err.message);
       return;
     }
-    console.log(data);
+    console.log('Connected to the database.');
+  });
+
+  db.all("SELECT name, sql FROM sqlite_master WHERE type='table'", [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    rows.forEach((row) => {
+      console.log(`Table: ${row.name}\nSQL: ${row.sql}\n`);
+    });
+  });
+
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    console.log('Closed the database connection.');
   });
 })
 
